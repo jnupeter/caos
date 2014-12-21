394e44525b4533a976b2a0295c4ba9abfc674ec8
Roger Wee <roger.wee@gmail.com>
2014-12-21 16:51:59 +1000

    Added in new My Details page and updated style guide.

--- src/main/webapp/resources/js/qtac-caos.js
+++ src/main/webapp/resources/js/qtac-caos.js
@@ -12,7 +12,7 @@ $(function() {
         $('body').toggleClass('hide-body');
         return false;
     });
-    $('#logout, .btn, .ui-button').button().click(function(){alert('button clicked');});
+    $('#logout, .btn, .ui-button').button().click(function(){console.log('button clicked');});
     $('#key-dates .arrow-down').click(function() {
         $('#key-dates .dates-container').animate({
             scrollTop: $('#key-dates div').offset().top
@@ -24,5 +24,23 @@ $(function() {
             scrollTop: 0
         }, 2000);
         return false;
+    });
+    $('.form-row').each(function(){
+       if($(this).find('.editable-field').length == 1) {
+           $(this).find('.inline-fields').hide();
+           var $this = $(this);
+           $(this).find('.edit-link').click(function(){
+               $this.find('.inline-fields').show();
+               $this.find('.edit-link').hide();
+               $this.find('.editable-field').hide();
+               return false;
+           });
+           $(this).find('.inline-fields a').click(function(){
+               $this.find('.inline-fields').hide();
+               $this.find('.edit-link').show();
+               $this.find('.editable-field').show();
+               return false;
+           });
+       }
     });
 });
